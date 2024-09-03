b1a9f6ec0d841132f3883b1adf64da10101beff2
furkan sahin <sahinffurkan@gmail.com>
2024-09-03 10:56:42 +0200

    initial

---
+++ cert-server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "golang.org/x/time/rate"
+)
+
+// Rate limiter allowing 10 requests per second
+var limiter = rate.NewLimiter(10, 1)
+
+func fileHandler(filename string) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        // Limit the request size
+        r.Body = http.MaxBytesReader(w, r.Body, 1024)
+
+        if !limiter.Allow() {
+            http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+            return
+        }
+
+        // Read the specified file
+        data, err := ioutil.ReadFile(filename)
+        if err != nil {
+            http.Error(w, "File not found", http.StatusNotFound)
+            return
+        }
+
+        // Write the file content to the response
+        w.Write(data)
+    }
+}
+
+func main() {
+    vmInhostName := os.Getenv("VM_INHOST_NAME")
+    if vmInhostName == "" {
+        log.Fatal("Environment variable vm_path is not set")
+    }
+
+    ipv6Address := os.Getenv("IPV6_ADDRESS")
+    if ipv6Address == "" {
+        log.Fatal("Environment variable ipv6_address is not set")
+    }
+
+    certFilePath := fmt.Sprintf("/vm/%s/cert/cert.pem", vmInhostName)
+    keyFilePath := fmt.Sprintf("/vm/%s/cert/key.pem", vmInhostName)
+
+    http.HandleFunc("/cert.pem", fileHandler(certFilePath))
+    http.HandleFunc("/key.pem", fileHandler(keyFilePath))
+
+    address := fmt.Sprintf("[%s]:8080", ipv6Address) // Format for IPv6
+
+    log.Printf("Server starting on %s...\n", address)
+    log.Fatal(http.ListenAndServe(address, nil))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module cert-server
+
+go 1.23
+
+require golang.org/x/time v0.6.0
